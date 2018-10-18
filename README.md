104e6bf09001ab4144abbe68ce0684638cd36d37
wangdabaoqq <614692339@qq.com>
2018-10-18 13:24:04 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 react-App
+i
--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>sss</div>
+        <Wangda wa='wa'></Wangda>
       </div>
     );
   }
 }
 
+class Wangda extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ddd: ['1', '2', '3']
+    }
+  }
+  clicks () {
+    this.setState({
+      ddd: [...this.state.ddd, '111']
+    })
+  }
+  render() {
+    return (
+      <div>
+      <div>vvv, {this.props.wa}</div>
+      <button onClick={()=>this.clicks()}></button>
+      <div>
+        {this.state.ddd.map((e) => {
+          return <li key={e}>{e}</li>
+        })}
+      </div>
+      </div>
+    );
+  }
+}
 export default App;
